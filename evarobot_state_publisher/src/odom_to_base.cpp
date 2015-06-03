53e6be76f75f79c8a617ed3b1fa00a17bd3150e8
Mehmet Akcakoca <akcakocamehmet@gmail.com>
2015-06-03 10:11:30 +0300

    modified robot state

---
+++ evarobot_state_publisher/src/odom_to_base.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include "tf/transform_broadcaster.h"
+#include "nav_msgs/Odometry.h"
+
+
+geometry_msgs::TransformStamped odom_trans;
+
+void Callback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+    odom_trans.header.stamp = ros::Time::now();
+    odom_trans.header.frame_id = msg->header.frame_id;
+    odom_trans.child_frame_id = msg->child_frame_id;
+
+    odom_trans.transform.translation.x = msg->pose.pose.position.x;
+    odom_trans.transform.translation.y = msg->pose.pose.position.y;
+    odom_trans.transform.translation.z = msg->pose.pose.position.z;
+    odom_trans.transform.rotation = msg->pose.pose.orientation;
+	
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "odom_to_base");
+
+  ros::NodeHandle n;
+ // ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  ros::Subscriber sub_odom = n.subscribe("odom", 1, Callback);
+  
+  tf::TransformBroadcaster odom_broadcaster;
+
+  ros::Rate r(100);
+  
+  while(n.ok())
+  {
+
+	// check for incoming messages
+    ros::spinOnce();               
+    
+    //send the transform
+    odom_broadcaster.sendTransform(odom_trans);
+
+   r.sleep();
+  }
+}
